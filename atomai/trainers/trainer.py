5326505eb2a1d2faaaa691299d1d27f65fc0d3ca
Hitesh Gupta <81859555+Hitesh1309@users.noreply.github.com>
2024-04-15 00:00:40 +0530

    Update trainer.py

--- atomai/trainers/trainer.py
+++ atomai/trainers/trainer.py
@@ -22,7 +22,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 from atomai import losses_metrics
-from atomai.nets import (init_cls_model, init_fcnn_model, init_imspec_model,
+from atomai.nets import (init_cls_model, init_fcnn_model, init_imspec_model, init_imspec_model_lstm,
                          init_reg_model)
 from atomai.utils import (array2list, average_weights, gpu_usage_map,
                           init_cls_dataloaders, init_dataloaders,
@@ -926,6 +926,128 @@ class ImSpecTrainer(BaseTrainer):
             raise AssertionError(
                 "The input/output dimensions of the model must match" +
                 " the height, width and length (for spectra) of training")
+
+##### EDIT FOR LSTM #####
+
+class ImSpecTrainerLSTM(BaseTrainer):
+    """
+    Trainer of neural network for image-to-spectrum
+    and spectrum-to-image transformations
+
+    Args:
+        in_dim:
+            Input data dimensions.
+            (height, width) for images or (length,) for spectra
+        out_dim:
+            output dimensions.
+            (length,) for spectra or (height, width) for images
+        latent_dim:
+            dimensionality of the latent space
+            (number of neurons in a fully connected bottleneck layer)
+        **seed (int):
+            Deterministic mode for model training (Default: 1)
+        **batch_seed (int):
+            Separate seed for generating a sequence of batches
+            for training/testing. Equal to 'seed' if set to None (default)
+        **nblayers_encoder (int):
+            number of convolutional layers in the encoder
+        **nblayers_decoder (int):
+            number of convolutional layers in the decoder
+        **nbfilters_encoder (int):
+            number of convolutional filters in each layer of the encoder
+        **nbfilters_decoder (int):
+            number of convolutional filters in each layer of the decoder
+        **batch_norm (bool):
+            Apply batch normalization after each convolutional layer
+            (Default: True)
+        **encoder_downsampling (int):
+            downsamples input data by this factor before passing
+            to convolutional layers (Default: no downsampling)
+        **decoder_upsampling (bool):
+            performs upsampling+convolution operation twice on the reshaped latent
+            vector (starting from image/spectra dims 4x smaller than the target dims)
+            before passing  to the decoder
+    """
+    def __init__(self,
+                 in_dim: Tuple[int],
+                 out_dim: Tuple[int],
+                 latent_dim: int = 2,
+                 **kwargs: Union[int, bool, str]) -> None:
+        super(ImSpecTrainer, self).__init__()
+        """
+        Initialize trainer's parameters
+        """
+        seed = kwargs.get("seed", 1)
+        kwargs["batch_seed"] = kwargs.get("batch_seed", seed)
+        set_train_rng(seed)
+
+        self.in_dim, self.out_dim = in_dim, out_dim
+        (self.net,
+         self.meta_state_dict) = init_imspec_model_lstm(in_dim, out_dim, latent_dim,
+                                                   **kwargs)
+
+        self.net.to(self.device)
+        self.meta_state_dict["weights"] = self.net.state_dict()
+        #self.meta_state_dict["optimizer"] = self.optimizer
+
+    def set_data(self,
+                 X_train: Union[np.ndarray, torch.Tensor],
+                 y_train: Union[np.ndarray, torch.Tensor],
+                 X_test: Optional[Union[np.ndarray, torch.Tensor]] = None,
+                 y_test: Optional[Union[np.ndarray, torch.Tensor]] = None,
+                 **kwargs: Union[float, int]) -> None:
+        """
+        Sets training and test data.
+
+        Args:
+            X_train:
+                4D numpy array or torch tensor with image data
+                (n_samples x 1 x height x width) or 3D array/tensor
+                with spectral data (n_samples x 1 x signal_length).
+                It is also possible to pass 3D and 2D arrays by ignoring
+                the channel dim of 1, which will be added automatically.
+                The X_train is typically referred to as 'features'
+            y_train:
+                3D numpy array or torch tensor with spectral data
+                (n_samples x 1 x signal_length) or 4D array/tensor with
+                image data (n_samples x 1 x height x width).
+                It is also possible to pass 2D and 3D arrays by ignoring
+                the channel dim of 1, which will be added automatically.
+                Note that if your X_train data are images,
+                then your y_train must be spectra and vice versa.
+                The y_train is typicaly referred to as "targets"
+            X_test:
+                Test data (features) of the same dimesnionality
+                (except for the number of samples) as X_train
+            y_test:
+                Test data (targets) of the same dimesnionality
+                (except for the number of samples) as y_train
+            kwargs:
+                Parameters for train_test_split ('test_size' and 'seed') when
+                separate test set is not provided and 'memory_alloc', which
+                sets a threshold (in GBs) for holding entire training data on GPU
+        """
+
+        if X_test is None or y_test is None:
+            X_train, X_test, y_train, y_test = train_test_split(
+                X_train, y_train, test_size=kwargs.get("test_size", .15),
+                shuffle=True, random_state=kwargs.get("seed", 1))
+
+        if self.full_epoch:
+            self.train_loader, self.test_loader, dims = init_imspec_dataloaders(
+                X_train, y_train, X_test, y_test,
+                self.batch_size, kwargs.get("memory_alloc", 4))
+        else:
+            (self.X_train, self.y_train,
+             self.X_test, self.y_test, dims) = preprocess_training_imspec_data(
+                X_train, y_train, X_test, y_test,
+                self.batch_size, kwargs.get("memory_alloc", 4))
+
+        if dims[0] != self.in_dim or dims[1] != self.out_dim:
+            raise AssertionError(
+                "The input/output dimensions of the model must match" +
+                " the height, width and length (for spectra) of training")
+##### EDIT FOR LSTM #####
 
 
 class RegTrainer(BaseTrainer):
