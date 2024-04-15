de8a26c3a0f96789447b54ba67fc796bbde8ca1d
Hitesh Gupta <81859555+Hitesh1309@users.noreply.github.com>
2024-04-15 06:39:49 +0530

    Update __init__.py

--- atomai/models/__init__.py
+++ atomai/models/__init__.py
@@ -1,12 +1,12 @@
 from .segmentor import Segmentor
-from .imspec import ImSpec
+from .imspec import ImSpec, ImSpecLSTM
 from .regressor import Regressor
 from .classifier import Classifier
 from .dgm import BaseVAE, VAE, rVAE, jVAE, jrVAE
 from .dklgp import dklGPR, Reconstructor
 from .loaders import load_model, load_ensemble, load_pretrained_model
 
-__all__ = ["Segmentor", "ImSpec", "BaseVAE", "VAE", "rVAE",
+__all__ = ["Segmentor", "ImSpec", "ImSpecLSTM", "BaseVAE", "VAE", "rVAE",
            "jVAE", "jrVAE", "load_model", "load_ensemble",
            "load_pretrained_model", "dklGPR", "Regressor",
            "Classifier", "Reconstructor"]
